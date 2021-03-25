1f8cc8ce8c6b6df31afccecdf9156a7919a70a31
adamek195 <248955@student.pwr.edu.pl>
2021-03-25 13:13:33 +0100

    remove spaces

--- Project1/main.py
+++ Project1/main.py
@@ -7,27 +7,27 @@ from SchragePmtn import schragePmtn
 
 def main():
     seed = int(input("Wprowadź Z:"))
-    generator = RandomNumberGenerator(seed) 
+    generator = RandomNumberGenerator(seed)
     taskNumber = int(input("Wprtowadź liczbe zadań:"))
     tasks = range(1, taskNumber+1)
- 
+
     nr = []  #wektor kolejnych zadan
     r = []  #czas przygotowania kolejnych zadań
     p = []  #czas wykonania kolejnych zadan
     q = []  #czas dostarczenia/stygnięcia kolejnych zadań
 
-    #generowanie danych do zadania 
-    for task in tasks:   
+    #generowanie danych do zadania
+    for task in tasks:
         nr.append(task)
         p.append(generator.nextInt(1,29))
 
-    pi = nr.copy() #inicjalizacja permutacji zadań 
-    
+    pi = nr.copy() #inicjalizacja permutacji zadań
+
     A = 0
     # X = 29
     for number in p:
         A += number
-    
+
     for task in tasks:
         r.append(generator.nextInt(1,A))
 
@@ -44,7 +44,7 @@ def main():
     print(f'pi: {pi}')
     print("S: ", S)
     print("C: ", C)
-    print(f"Cq: {Cq}")   
+    print(f"Cq: {Cq}")
     print("Cmax", Cmax)
     print("\n")
 
@@ -52,17 +52,17 @@ def main():
     #inicjalizacja permutacji zadań
     # pi = nr.copy()
 
-    #strutktura skladajaca sie z 
+    #strutktura skladajaca sie z
     solution = []
-    for task in tasks: 
+    for task in tasks:
         solution.append([pi[task-1], r[task-1], p[task-1], q[task-1]])
 
-    #Schrage 
+    #Schrage
     print("Po sortowaniu:")
-    pi = schrage(r, p, q, tasks)
+    pi = schragePmtn(r, p, q, tasks)
     sort = {x: i for i, x in enumerate(pi)}
     solution.sort(key = lambda x: sort[x[0]])
-    
+
     rjSchrage = []
     pjSchrage = []
     qjSchrage = []
@@ -72,13 +72,13 @@ def main():
         pjSchrage.append(solution[task-1][2])
         qjSchrage.append(solution[task-1][3])
 
-    SSchrage, CSchrage, CqSchrage, CmaxSchrager = calculate(rjSchrage, pjSchrage, qjSchrage, taskNumber) 
-    print("pi:", pi)
-    print("S: ", SSchrage)
-    print("C: ", CSchrage)
-    print(f"Cq: {CqSchrage}")     
+    SSchrage, CSchrage, CqSchrage, CmaxSchrager = calculate(rjSchrage, pjSchrage, qjSchrage, taskNumber)
+    #print("pi:", pi)
+    #print("S: ", SSchrage)
+    #print("C: ", CSchrage)
+    #print(f"Cq: {CqSchrage}")
     print("Cmax", CmaxSchrager)
-    
+
 
 if __name__ == '__main__':
 	main()
